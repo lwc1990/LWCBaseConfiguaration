dbbf7e35e9d9ddd4b3bb3d61ab0d0a3fc627a802
luwenchao <lwc_funnylife@126.com>
2019-05-11 17:44:49 +0800

    添加文件

---
+++ LWCBaseConfiguaration/Classes/model/LWCBaseConfiguaration.h
@@ -0,0 +1,16 @@
+//
+//  LWCBaseConfiguaration.h
+//  Pods-LWCBaseConfiguaration_Example
+//
+//  Created by cheyipai on 2019/5/11.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LWCBaseConfiguaration : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LWCBaseConfiguaration/Classes/model/LWCBaseConfiguaration.m
@@ -0,0 +1,12 @@
+//
+//  LWCBaseConfiguaration.m
+//  Pods-LWCBaseConfiguaration_Example
+//
+//  Created by cheyipai on 2019/5/11.
+//
+
+#import "LWCBaseConfiguaration.h"
+
+@implementation LWCBaseConfiguaration
+
+@end
